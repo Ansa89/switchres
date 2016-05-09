e0bb19f1d580d8136baca140e41b45aae1772531
Ansa89 <ansalonistefano@gmail.com>
2016-05-09 13:16:32 +0200

    version.sh: simplify versioning

--- version.sh
+++ version.sh
@@ -1,34 +1,21 @@
 #!/bin/sh
 
-VERSION="1"
-VER="53"
-DATE=`date`
-SYSTEM=`uname -a`
-MACHINE=`uname -m`
-OS=`uname`
-LOCALVER=0
+VERSION=1
+REV=53
+DATE=$(date)
+SYSTEM=$(uname -a)
+MACHINE=$(uname -m)
+OS=$(uname)
 
 if [[ -d ".git" ]] || [[ -d "../../.git" ]]; then
-    git rev-list HEAD | sort > config.git-hash
-    LOCALVER=`wc -l config.git-hash | awk '{print $1}'`
-fi
-if [[ $LOCALVER > 1 ]] ; then
-    VER=`git rev-list origin/master | sort | join config.git-hash - | wc -l | awk '{print $1}'`
-    if [ $VER != $LOCALVER ] ; then
-        VER="$VER+$(($LOCALVER-$VER))"
-    elif git status | grep -q "modified:" ; then
-        VER="${VER}M"
-    fi
-    VER="$VER-$(git rev-list HEAD -n 1 | head -c 7)"
-    echo "#define VERSION \"${VERSION}.${VER}\"" > version.h
-    rm -f config.git-hash
+    REV="$REV~$(git rev-list HEAD -n 1 | head -c 7)"
+    echo "#define VERSION \"${VERSION}.${REV}\"" > version.h
 else
-    echo "#define VERSION \"${VERSION}.${VER}\"" > version.h
-    VER="x"
+    echo "#define VERSION \"${VERSION}.${REV}\"" > version.h
 fi
-echo "#define NUMBER_VERSION \"$VERSION.$VER\"" >>version.h
-echo "#define SYSTEM_VERSION \"$SYSTEM\"" >>version.h
-echo "#define BUILD_DATE \"$DATE\"" >>version.h
-echo "#define MACHINE \"$MACHINE\"" >>version.h
-echo "#define OS \"$OS\"" >>version.h
+echo "#define NUMBER_VERSION \"${VERSION}.${REV}\"" >> version.h
+echo "#define SYSTEM_VERSION \"$SYSTEM\"" >> version.h
+echo "#define BUILD_DATE \"$DATE\"" >> version.h
+echo "#define MACHINE \"$MACHINE\"" >> version.h
+echo "#define OS \"$OS\"" >> version.h
 
