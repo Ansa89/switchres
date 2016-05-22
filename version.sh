37a9fe9dc9cc07d2874ca26988cc18645ce3e8c2
Ansa89 <ansalonistefano@gmail.com>
2016-05-22 15:30:02 +0200

    version.sh: remove redundant code

--- version.sh
+++ version.sh
@@ -13,11 +13,9 @@ if [[ -d ".git" ]] || [[ -d "../../.git" ]]; then
         REV="${REV}M"
     fi
     REV="${REV}$(git rev-list HEAD -n 1 | head -c 7)"
-    echo "#define VERSION \"${VERSION}.${REV}\"" > version.h
-else
-    echo "#define VERSION \"${VERSION}.${REV}\"" > version.h
 fi
 
+echo "#define VERSION \"${VERSION}.${REV}\"" > version.h
 echo "#define NUMBER_VERSION \"${VERSION}.${REV}\"" >> version.h
 echo "#define SYSTEM_VERSION \"$SYSTEM\"" >> version.h
 echo "#define BUILD_DATE \"$DATE\"" >> version.h
